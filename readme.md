e4a3303a5a10e30871f3b391ce9c3552c0743bf7
Apricityx <apricityx@qq.com>
2024-03-01 08:44:06 +0800

    取消中间件，改为监听on message

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
-# koishi-plugin-repeater
+# koishi-plugin-new-repeater
 
-[![npm](https://img.shields.io/npm/v/koishi-plugin-repeater?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-repeater)
+[![npm](https://img.shields.io/npm/v/koishi-plugin-new-repeater?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-new-repeater)
 
-A simple repeater.
+简单的复读🐔 可以自定义复读内容和复读概率
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,40 @@
-import { Context, Schema } from 'koishi'
+import {Context, Schema} from 'koishi'
 
-export const name = 'repeater'
+export const name = 'new-repeater'
 
-export interface Config {}
+export interface Config {
+}
 
+export const description = '复读机器人'
 export const Config: Schema<Config> = Schema.object({
-  content: Schema.string().description('复读内容：').required(),
-  debug: Schema.boolean().description('是否启动调试模式').default(false),
-  possibility: Schema.number().description('复读概率').default(100),
+    content: Schema.string().description('复读内容：').required(),
+    debug: Schema.boolean().description('是否启动调试模式').default(false),
+    possibility: Schema.number().description('复读概率').default(100),
 })
 
 export function apply(ctx: Context, config: Config) {
-  const logger = ctx.logger('Repeater') // 创建logger用于输出Koishi日志
-  var ifDebug = false;
-  ifDebug = config["debug"];
-  function debug(content:any){
-    if (ifDebug)
-    logger.info(content);
-  }
-  const content = config["content"]
-  ctx.middleware((session, next) => {
-    if (session.content === content) {
-      // 随机复读
-      let random = Math.floor(Math.random() * 100);
-      if (random > config["possibility"]) {
-        debug(`懒得复读：${session.content}`);
-        return next()
-      }
-      else{
-        debug(`正在复读：${session.content}`);
-        return content
-      }
-    } else {
-      return next()
+    const logger = ctx.logger('new-repeater') // 创建logger用于输出Koishi日志
+    var ifDebug = false;
+    ifDebug = config["debug"];
+
+    function debug(content: any) {
+        if (ifDebug)
+            logger.info(content);
     }
-  })
+
+    const content = config["content"]
+    debug(`复读机启动完毕，复读内容：${content}`);
+    ctx.on('message', (session) => {
+        debug(`收到消息：${session.content}`)
+        if (session.content === content) {
+            // 随机复读
+            let random = Math.floor(Math.random() * 100);
+            if (random > config["possibility"]) {
+                debug(`懒得复读：${session.content}`);
+            } else {
+                debug(`正在复读：${session.content}`);
+                session.send(content)
+            }
+        }
+    })
 }
